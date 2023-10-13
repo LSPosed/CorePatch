6f2790c8cb7159f49476f5539ace71329a4f9a9a
Wang Han <416810799@qq.com>
2023-10-13 20:29:22 +0800

    Update for U compatibility and bump version to 4.3

--- app/src/main/java/toolkit/coderstory/CorePatchForSv2.java
+++
@@ -1,4 +0,0 @@
-package toolkit.coderstory;
-
-public class CorePatchForSv2 extends CorePatchForS{
-}
--- app/src/main/java/toolkit/coderstory/CorePatchForT.java
+++ app/src/main/java/toolkit/coderstory/CorePatchForT.java
@@ -9,7 +9,7 @@ import de.robv.android.xposed.XposedBridge;
 import de.robv.android.xposed.XposedHelpers;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
 
-public class CorePatchForT extends CorePatchForSv2 {
+public class CorePatchForT extends CorePatchForS {
     @Override
     public void handleLoadPackage(XC_LoadPackage.LoadPackageParam loadPackageParam) throws IllegalAccessException, InvocationTargetException, InstantiationException {
         super.handleLoadPackage(loadPackageParam);
@@ -29,8 +29,10 @@ public class CorePatchForT extends CorePatchForSv2 {
                 // Don't handle PERMISSION (grant SIGNATURE permissions to pkgs with this cert)
                 // Or applications will have all privileged permissions
                 // https://cs.android.com/android/platform/superproject/+/master:frameworks/base/core/java/android/content/pm/PackageParser.java;l=5947?q=CertCapabilities
-                if (((Integer) param.args[1] != 4) && prefs.getBoolean("digestCreak", true)) {
-                    param.setResult(true);
+                if (prefs.getBoolean("digestCreak", true)) {
+                    if ((Integer) param.args[1] != 4) {
+                        param.setResult(true);
+                    }
                 }
             }
         });
