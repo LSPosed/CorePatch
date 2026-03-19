1a485cc529b4531b0cd6611c601ae5b54549c12d
Js0n <29531167+JasonKhew96@users.noreply.github.com>
2026-03-19 20:30:55 +0800

    Force enable ALLOW_NON_PRELOADS_SYSTEM_SHAREDUIDS on U+ (#126)
    Co-authored-by: Wang Han <416810799@qq.com>

--- app/src/main/java/org/lsposed/corepatch/hook/ReconcilePackageUtilsHook.kt
+++ app/src/main/java/org/lsposed/corepatch/hook/ReconcilePackageUtilsHook.kt
@@ -2,6 +2,7 @@ package org.lsposed.corepatch.hook
 
 import android.annotation.SuppressLint
 import android.os.Build
+import org.lsposed.corepatch.Config
 import org.lsposed.corepatch.XposedHelper
 import org.lsposed.corepatch.XposedHelper.hostClassLoader
 import org.lsposed.corepatch.XposedHelper.log
@@ -13,10 +14,19 @@ object ReconcilePackageUtilsHook : BaseHook() {
     override fun hook() {
         if (Build.VERSION.SDK_INT < Build.VERSION_CODES.TIRAMISU) return
 
+        // https://cs.android.com/android/platform/superproject/+/android-14.0.0_r75:frameworks/base/services/core/java/com/android/server/pm/ReconcilePackageUtils.java
         val reconcilePackageUtilsClazz =
             hostClassLoader.loadClass("com.android.server.pm.ReconcilePackageUtils")
         val reconcilePackagesMethod =
             reconcilePackageUtilsClazz.declaredMethods.first { m -> m.name == "reconcilePackages" }
         if (!XposedHelper.deoptimize(reconcilePackagesMethod)) log("failed to deoptimize reconcilePackages")
+
+        if (Config.isBypassDigestEnabled() && !Config.isBypassSharedUserEnabled()) {
+            reconcilePackageUtilsClazz.declaredFields.firstOrNull { field -> field.name == "ALLOW_NON_PRELOADS_SYSTEM_SHAREDUIDS" }
+                ?.let { field ->
+                    field.isAccessible = true
+                    field.set(null, true)
+                }
+        }
     }
 }
